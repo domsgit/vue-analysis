e20bdc187a9ced11be78fe3d828b900f66e8b4ac
domsgit <domsgit@outlook.com>
2019-10-29 20:47:47 +0800

    feat: add src/core/global-api dir

---
+++ src/core/global-api/assets.js
@@ -0,0 +1,37 @@
+/* @flow */
+
+import { ASSET_TYPES } from 'shared/constants'
+import { isPlainObject, validateComponentName } from '../util/index'
+
+export function initAssetRegisters (Vue: GlobalAPI) {
+  /**
+   * Create asset registration methods.
+   * 创建asset注册函数。asset包含：组件component，过滤filter，指令directive
+   */
+  ASSET_TYPES.forEach(type => {
+    Vue[type] = function (
+      id: string,
+      definition: Function | Object
+    ): Function | Object | void {
+      if (!definition) {
+        return this.options[type + 's'][id]
+      } else {
+        /* istanbul ignore if */
+        if (process.env.NODE_ENV !== 'production' && type === 'component') {
+          // 如果是生产环境，创建的事component注册函数，则需要校验组件名是否有效
+          validateComponentName(id)
+        }
+        if (type === 'component' && isPlainObject(definition)) {
+          definition.name = definition.name || id
+          definition = this.options._base.extend(definition)
+        }
+        if (type === 'directive' && typeof definition === 'function') {
+          definition = { bind: definition, update: definition }
+        }
+        // 将definition挂在Vue的options上
+        this.options[type + 's'][id] = definition
+        return definition
+      }
+    }
+  })
+}
---
+++ src/core/global-api/use.js
@@ -0,0 +1,31 @@
+/* @flow */
+
+import { toArray } from '../util/index'
+
+export function initUse (Vue: GlobalAPI) {
+  Vue.use = function (plugin: Function | Object) {
+    // 已经安装了的插件有哪些？（一开始时为空数组）
+    const installedPlugins = (this._installedPlugins || (this._installedPlugins = []))
+    // 如果该插件已经安装了，则直接返回，不继续往下走
+    if (installedPlugins.indexOf(plugin) > -1) {
+      // 返回Vue，以便链式调用
+      return this
+    }
+
+    // additional parameters
+    // 附加的参数类数组转为数组
+    const args = toArray(arguments, 1)
+    // 转成的数组参数前添加Vue，以便拿到Vue及其原型上的属性和方法
+    args.unshift(this)
+    // 执行插件
+    if (typeof plugin.install === 'function') {
+      plugin.install.apply(plugin, args)
+    } else if (typeof plugin === 'function') {
+      plugin.apply(null, args)
+    }
+    // 把该插件归入已安装的插件数组
+    installedPlugins.push(plugin)
+    // 返回Vue，以便链式调用
+    return this
+  }
+}
